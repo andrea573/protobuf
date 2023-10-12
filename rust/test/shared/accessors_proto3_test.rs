4be31becc36bf56aae744e8c6a7cadb27136a2f0
Hong Shin <hongshin@google.com>
2023-10-12 16:08:46 -0700

    Perform preliminary migration of accessors_proto3_test to googletest-rust
    PiperOrigin-RevId: 573038031

--- rust/test/shared/accessors_proto3_test.rs
+++ rust/test/shared/accessors_proto3_test.rs
@@ -5,6 +5,7 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
+use googletest::prelude::*;
 /// Tests covering accessors for singular bool, int32, int64, and bytes fields
 /// on proto3.
 use protobuf::Optional;
@@ -14,31 +15,31 @@ use unittest_proto3_optional::proto2_unittest::TestProto3Optional;
 #[test]
 fn test_fixed32_accessors() {
     let mut msg = TestAllTypes::new();
-    assert_eq!(msg.optional_fixed32(), 0);
-    assert_eq!(msg.optional_fixed32_mut().get(), 0);
+    assert_that!(msg.optional_fixed32(), eq(0));
+    assert_that!(msg.optional_fixed32_mut().get(), eq(0));
 
     msg.optional_fixed32_mut().set(42);
-    assert_eq!(msg.optional_fixed32_mut().get(), 42);
-    assert_eq!(msg.optional_fixed32(), 42);
+    assert_that!(msg.optional_fixed32_mut().get(), eq(42));
+    assert_that!(msg.optional_fixed32(), eq(42));
 
     msg.optional_fixed32_mut().clear();
-    assert_eq!(msg.optional_fixed32(), 0);
-    assert_eq!(msg.optional_fixed32_mut().get(), 0);
+    assert_that!(msg.optional_fixed32(), eq(0));
+    assert_that!(msg.optional_fixed32_mut().get(), eq(0));
 }
 
 #[test]
 fn test_bool_accessors() {
     let mut msg = TestAllTypes::new();
-    assert!(!msg.optional_bool());
-    assert!(!msg.optional_bool_mut().get());
+    assert_that!(msg.optional_bool(), eq(false));
+    assert_that!(msg.optional_bool_mut().get(), eq(false));
 
     msg.optional_bool_mut().set(true);
-    assert!(msg.optional_bool());
-    assert!(msg.optional_bool_mut().get());
+    assert_that!(msg.optional_bool(), eq(true));
+    assert_that!(msg.optional_bool_mut().get(), eq(true));
 
     msg.optional_bool_mut().clear();
-    assert!(!msg.optional_bool());
-    assert!(!msg.optional_bool_mut().get());
+    assert_that!(msg.optional_bool(), eq(false));
+    assert_that!(msg.optional_bool_mut().get(), eq(false));
 }
 
 #[test]
@@ -46,73 +47,74 @@ fn test_bytes_accessors() {
     let mut msg = TestAllTypes::new();
     // Note: even though it's named 'optional_bytes', the field is actually not
     // proto3 optional, so it does not support presence.
-    assert_eq!(msg.optional_bytes(), b"");
-    assert_eq!(msg.optional_bytes_mut().get(), b"");
+    assert_that!(*msg.optional_bytes(), empty());
+    assert_that!(*msg.optional_bytes_mut().get(), empty());
 
     msg.optional_bytes_mut().set(b"accessors_test");
-    assert_eq!(msg.optional_bytes(), b"accessors_test");
-    assert_eq!(msg.optional_bytes_mut().get(), b"accessors_test");
+    assert_that!(msg.optional_bytes(), eq(b"accessors_test"));
+    assert_that!(msg.optional_bytes_mut().get(), eq(b"accessors_test"));
 
     {
         let s = Vec::from(&b"hello world"[..]);
         msg.optional_bytes_mut().set(&s[..]);
     }
-    assert_eq!(msg.optional_bytes(), b"hello world");
-    assert_eq!(msg.optional_bytes_mut().get(), b"hello world");
+    assert_that!(msg.optional_bytes(), eq(b"hello world"));
+    assert_that!(msg.optional_bytes_mut().get(), eq(b"hello world"));
 
     msg.optional_bytes_mut().clear();
-    assert_eq!(msg.optional_bytes(), b"");
-    assert_eq!(msg.optional_bytes_mut().get(), b"");
+    assert_that!(*msg.optional_bytes(), empty());
+    assert_that!(*msg.optional_bytes_mut().get(), empty());
 
     msg.optional_bytes_mut().set(b"");
-    assert_eq!(msg.optional_bytes(), b"");
-    assert_eq!(msg.optional_bytes_mut().get(), b"");
+    assert_that!(*msg.optional_bytes(), empty());
+    assert_that!(*msg.optional_bytes_mut().get(), empty());
 }
 
 #[test]
 fn test_optional_bytes_accessors() {
     let mut msg = TestProto3Optional::new();
-    assert_eq!(msg.optional_bytes(), b"");
-    assert_eq!(msg.optional_bytes_opt(), Optional::Unset(&b""[..]));
-    assert_eq!(msg.optional_bytes_mut().get(), b"");
+    assert_that!(*msg.optional_bytes(), empty());
+    assert_that!(msg.optional_bytes_opt(), eq(Optional::Unset(&b""[..])));
+    assert_that!(*msg.optional_bytes_mut().get(), empty());
+    // TODO: update with custom set/unset after cl/572396117
     assert!(msg.optional_bytes_mut().is_unset());
 
     {
         let s = Vec::from(&b"hello world"[..]);
         msg.optional_bytes_mut().set(&s[..]);
     }
-    assert_eq!(msg.optional_bytes(), b"hello world");
-    assert_eq!(msg.optional_bytes_opt(), Optional::Set(&b"hello world"[..]));
+    assert_that!(msg.optional_bytes(), eq(b"hello world"));
+    assert_that!(msg.optional_bytes_opt(), eq(Optional::Set(&b"hello world"[..])));
     assert!(msg.optional_bytes_mut().is_set());
-    assert_eq!(msg.optional_bytes_mut().get(), b"hello world");
+    assert_that!(msg.optional_bytes_mut().get(), eq(b"hello world"));
 
     msg.optional_bytes_mut().or_default().set(b"accessors_test");
-    assert_eq!(msg.optional_bytes(), b"accessors_test");
-    assert_eq!(msg.optional_bytes_opt(), Optional::Set(&b"accessors_test"[..]));
+    assert_that!(msg.optional_bytes(), eq(b"accessors_test"));
+    assert_that!(msg.optional_bytes_opt(), eq(Optional::Set(&b"accessors_test"[..])));
     assert!(msg.optional_bytes_mut().is_set());
-    assert_eq!(msg.optional_bytes_mut().get(), b"accessors_test");
-    assert_eq!(msg.optional_bytes_mut().or_default().get(), b"accessors_test");
+    assert_that!(msg.optional_bytes_mut().get(), eq(b"accessors_test"));
+    assert_that!(msg.optional_bytes_mut().or_default().get(), eq(b"accessors_test"));
 
     msg.optional_bytes_mut().clear();
-    assert_eq!(msg.optional_bytes(), b"");
-    assert_eq!(msg.optional_bytes_opt(), Optional::Unset(&b""[..]));
+    assert_that!(*msg.optional_bytes(), empty());
+    assert_that!(msg.optional_bytes_opt(), eq(Optional::Unset(&b""[..])));
     assert!(msg.optional_bytes_mut().is_unset());
 
     msg.optional_bytes_mut().set(b"");
-    assert_eq!(msg.optional_bytes(), b"");
-    assert_eq!(msg.optional_bytes_opt(), Optional::Set(&b""[..]));
+    assert_that!(*msg.optional_bytes(), empty());
+    assert_that!(msg.optional_bytes_opt(), eq(Optional::Set(&b""[..])));
 
     msg.optional_bytes_mut().clear();
     msg.optional_bytes_mut().or_default();
-    assert_eq!(msg.optional_bytes(), b"");
-    assert_eq!(msg.optional_bytes_opt(), Optional::Set(&b""[..]));
+    assert_that!(*msg.optional_bytes(), empty());
+    assert_that!(msg.optional_bytes_opt(), eq(Optional::Set(&b""[..])));
 
     msg.optional_bytes_mut().or_default().set(b"\xffbinary\x85non-utf8");
-    assert_eq!(msg.optional_bytes(), b"\xffbinary\x85non-utf8");
-    assert_eq!(msg.optional_bytes_opt(), Optional::Set(&b"\xffbinary\x85non-utf8"[..]));
+    assert_that!(msg.optional_bytes(), eq(b"\xffbinary\x85non-utf8"));
+    assert_that!(msg.optional_bytes_opt(), eq(Optional::Set(&b"\xffbinary\x85non-utf8"[..])));
     assert!(msg.optional_bytes_mut().is_set());
-    assert_eq!(msg.optional_bytes_mut().get(), b"\xffbinary\x85non-utf8");
-    assert_eq!(msg.optional_bytes_mut().or_default().get(), b"\xffbinary\x85non-utf8");
+    assert_that!(msg.optional_bytes_mut().get(), eq(b"\xffbinary\x85non-utf8"));
+    assert_that!(msg.optional_bytes_mut().or_default().get(), eq(b"\xffbinary\x85non-utf8"));
 }
 
 #[test]
@@ -187,20 +189,20 @@ fn test_oneof_accessors() {
     use TestAllTypes_::OneofField::*;
 
     let mut msg = TestAllTypes::new();
-    assert!(matches!(msg.oneof_field(), not_set(_)));
+    assert_that!(msg.oneof_field(), matches_pattern!(not_set(_)));
 
     msg.oneof_uint32_set(Some(7));
-    assert_eq!(msg.oneof_uint32_opt(), Optional::Set(7));
-    assert!(matches!(msg.oneof_field(), OneofUint32(7)));
+    assert_that!(msg.oneof_uint32_opt(), eq(Optional::Set(7)));
+    assert_that!(msg.oneof_field(), matches_pattern!(OneofUint32(eq(7))));
 
     msg.oneof_uint32_set(None);
-    assert_eq!(msg.oneof_uint32_opt(), Optional::Unset(0));
-    assert!(matches!(msg.oneof_field(), not_set(_)));
+    assert_that!(msg.oneof_uint32_opt(), eq(Optional::Unset(0)));
+    assert_that!(msg.oneof_field(), matches_pattern!(not_set(_)));
 
     msg.oneof_uint32_set(Some(7));
     msg.oneof_bytes_mut().set(b"");
-    assert_eq!(msg.oneof_uint32_opt(), Optional::Unset(0));
+    assert_that!(msg.oneof_uint32_opt(), eq(Optional::Unset(0)));
 
     // This should show it set to the OneofBytes but its not supported yet.
-    assert!(matches!(msg.oneof_field(), not_set(_)));
+    assert_that!(msg.oneof_field(), matches_pattern!(not_set(_)));
 }
